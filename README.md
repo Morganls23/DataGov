74c0e2ba82fc0cd260a7ec72f434afeeb6b77421
Morgan <morgan.smith118@gmail.com>
2020-04-15 14:55:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+This profile provides a base configuration for PingDataGovernance. It stands up a complete DG stack with the Policy Admin server and the Jokes API configuration that is outlined in the documentation (https://docs.pingidentity.com/bundle/pingdatagovernance-80/page/gkj1564011508864.html)
+
+It is designed to be used in conjunction with:
+* PD-Base Profile (https://github.com/cprice-ping/PD-Base)
+* PF-Base Profile (https://github.com/cprice-ping/PF-Base)
+
+(This is because the DG Access Token Validator is configured for a `PingIntrospect` OAuth client)
+
+## Deployment
+* Copy the `docker-compose.yaml` and `env_vars` files to a folder
+* Modify the `env_vars` file to match your environment
+* Launch the stack with `docker-compose up -d`
+---
+## Manual Steps
+**PAP Licensing**  
+
+Inject a DG license into PAP
+* The license file does not get pulled from DevOps
+* Place a valid DG 8 license file into your local volume that's referenced in the `docker-compose.yaml` file  
+
+**Add the Gateway Policy to PAP**  
+There's a Postman Newman service in this stack that calls a Postman collection to inject the `gatewayPolicyExample.SNAPSHOT` contents into PAP. 
+
+Test the Joke API (Call goes through DataGov)
+* `curl -k -X GET https://{pingdatagov}:7443/jokes/random   -H 'Authorization: Bearer {"active": true}'`  
+Should get a `status:200`  and a joke returned
+* `curl -k -X GET https://{pingdatagov}:7443/jokes/random   -H 'Authorization: Bearer {"active": false}'`  
+Should get a `status:401` and an error about an Invalid Token
---
+++ pap/Build/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+#
+# Ping Identity DevOps - Docker Build Hooks
+#
+
+    echo "
+##################################################################################
+#               CPrice - PingDataGov - PAP installer
+##################################################################################
+# 
+#     Configured with the following values.  
+# 
+#       PAP_HOST: ${PAP_HOST}
+#       PAP_SECRET: ${PAP_SECRET}
+# 
+#     To set via a docker run or .yaml just set them using examples below
+#
+#    docker run
+#           ...
+#           --env PAP_HOST=myhost.mydomain.com
+#           ...
+#
+##################################################################################
+"
+
+    cd /opt/pap
+    unzip PingDataGovernance-PAP-8.0.0.0.zip
+
+    cd /opt/pap/PingDataGovernance-PAP || echo "Unable to cd to the PAP bin directory"
+
+    echo "
+######################
+Running Command: bin/setup demo --licenseKeyFile /opt/pap/PingDataGovernance-PAP/PingDataGovernance.lic --generateSelfSignedCertificate --certNickname server-cert --pkcs12KeyStorePath config/keystore.p12 --hostname ${PAP_HOST} --port 9443 --decisionPointSharedSecret ${PAP_SECRET}
+######################
+"
+
+    bin/setup demo --licenseKeyFile /opt/pap/PingDataGovernance-PAP/PingDataGovernance.lic --generateSelfSignedCertificate --certNickname server-cert --pkcs12KeyStorePath config/keystore.p12 --hostname ${PAP_HOST} --port 9443 --decisionPointSharedSecret ${PAP_SECRET}
+
+    echo "
+    #################################"
+    grep "Authentication.SharedSecret:" /opt/pap/PingDataGovernance-PAP/config/configuration.yml
+    echo "
+    #################################
+    "
+
+    java -Xmx1G -XX:+UseG1GC -Dsymphonic.Database.H2.Path=/opt/pap/PingDataGovernance-PAP/admin-point-application/db/ -classpath /opt/pap/PingDataGovernance-PAP/admin-point-application/lib/*:/opt/pap/PingDataGovernance-PAP/admin-point-application/bin/* com.symphonicsoft.adminpoint.AdministrationPointApplication server /opt/pap/PingDataGovernance-PAP/config/configuration.yml
